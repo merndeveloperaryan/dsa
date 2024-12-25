192d150a8b3a32a3721e072141fdfb97c6034311
merndeveloperaryan <merndeveloperaryan@gmail.com>
2024-12-25 11:12:35 +0530

    I understand some fundamentals of java

--- JAVA/operatorsMathApi/MathOperatorsApis.java
+++ JAVA/operatorsMathApi/MathOperatorsApis.java
@@ -1,5 +1,17 @@
 package JAVA.operatorsMathApi;
 
 public class MathOperatorsApis {
-    
+
+    public static void main(String[] args) {
+
+        double a = 12;
+        double b = 13;
+        double c = 15;
+
+        double s = (a + b + c) / 2;
+        double area = Math.sqrt((s * (s - a) * (s - b) * (s - c)));
+        
+        System.out.println(area);
+    }
+
 }
--- JAVA/variablesRules/Variables.java
+++ JAVA/variablesRules/Variables.java
@@ -2,5 +2,9 @@ public class Variables {
     
     public static void main(String[] args) {
         
+        int age = 19;
+        String name = "Aryan";
+        System.out.println("My name is " + name + " and my age is " + age);
+        
     }
 }
